b86f07d3843fe2ca571b708258f77795b18e4811
lluu0029 <lluu0029@student.monash.edu>
2025-08-28 18:39:51 +1000

    Updated linking

--- link_grain_track.py
+++ link_grain_track.py
@@ -35,24 +35,35 @@ with open(r"Grain Track Data/step56_track_result_dict.pickle", "rb") as f:
 with open(r"Grain Track Data/step67_track_result_dict.pickle", "rb") as f:
     step6 = pickle.load(f)
 
-
 steps = [step1, step2, step3, step4, step5, step6]
+
 merged_step_maps = []
+confident_tracks = []
+retrack_results = []
+trip_start_grains = []
 
 for step_data in steps:
     confident_track = convert_to_dict(step_data['confident_track'])
     retrack_result = convert_to_dict(step_data['retrack_result'])
     # split = convert_to_dict(step_data['split_grains'])
-    trip_start_grains = convert_to_dict(step_data['trip_start_grains'])
+    trip_start_grain = convert_to_dict(step_data['trip_start_grains'])
     # position = convert_to_dict(step_data['track_based_on_position'])
     
+    confident_tracks.append(confident_track)
+    retrack_results.append(retrack_result)
+    trip_start_grains.append(trip_start_grain)
+
     merged_map = {}
-    for d in [confident_track, retrack_result, trip_start_grains]: 
-        merged_map.update(d) # Merge confident_track, retrack_result and trip_start_grains into single dictionary
+    for d in [confident_track, retrack_result, trip_start_grain]: 
+        for id, linked_id in d.items(): # Merge confident_track, retrack_result and trip_start_grains into single dictionary
+            if id in merged_map:
+                merged_map[id].extend(linked_id) # If a link to id already exists, add the new linked_id.
+            else:
+                merged_map[id] = linked_id[:] # Set key (id) to copy of linked_id list 
+
     merged_step_maps.append(merged_map)
 
-
-full_grain_map = {}
+linked_grain_map = {}
 
 for initial_id in merged_step_maps[0].keys():
     grain_id_links = [] # Stores all subsequent linked grain ids for initial_id
@@ -71,10 +82,9 @@ for initial_id in merged_step_maps[0].keys():
         grain_id_links.append(next_grain_ids) # Add links for this step to grain_id_links
         curr_grain_ids = next_grain_ids
 
-    full_grain_map[initial_id] = grain_id_links 
+    linked_grain_map[initial_id] = grain_id_links 
 
+print(linked_grain_map)
 
-print(full_grain_map)
-
-with open("full_grain_map.pkl", "wb") as f:
-    pickle.dump(full_grain_map, f)
+with open("linked_grain_map.pkl", "wb") as f:
+    pickle.dump(linked_grain_map, f)
